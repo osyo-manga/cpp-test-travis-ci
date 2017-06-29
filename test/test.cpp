afc58300e342e45beac00e536a0fb9795b75ffcb
manga_osyo <manga.osyo@gmail.com>
2017-06-29 10:23:18 +0900

    Update.

--- test/test.cpp
+++ test/test.cpp
@@ -2,14 +2,20 @@
 #include "./catch.hpp"
 
 #include <vector>
+#include <list>
 
-TEST_CASE("test", "std::vector"){
-	std::vector<int> v{};
+template<typename List>
+void func(){
+	List v;
 	v.push_back(1);
 	v.push_back(2);
 	v.push_back(3);
 
-	REQUIRE(( v == std::vector<int>{1, 2, 3} ));
+	REQUIRE((v == List{1, 2, 3}));
+	REQUIRE((v == List{1, 2, 5}));
 }
 
-
+TEST_CASE("test", "STL Container"){
+	func<std::vector<int>>();
+	func<std::list<int>>();
+}
