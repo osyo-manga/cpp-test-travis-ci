07484dbd3579ee6dfa9c41a77e509dd8af5189cf
manga_osyo <manga.osyo@gmail.com>
2017-06-29 10:41:21 +0900

    Fix

--- test/test.cpp
+++ test/test.cpp
@@ -7,15 +7,31 @@
 template<typename List>
 void func(){
 	List v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
+	
+	SECTION("List.push_back"){
+		v.push_back(1);
+		v.push_back(2);
+		v.push_back(3);
 
-	REQUIRE((v == List{1, 2, 3}));
-	REQUIRE((v == List{1, 2, 5}));
+		REQUIRE((v == List{1, 2, 3}));
+	}
+
+	v.resize(3);
+    REQUIRE( v.size() == 5 );
+
+    SECTION( "resizing bigger changes size and capacity" ) {
+        v.resize( 10 );
+
+        REQUIRE( v.size() == 10 );
+    }
+    SECTION( "resizing smaller changes size but not capacity" ) {
+        v.resize( 0 );
+
+        REQUIRE( v.size() == 0 );
+    }
 }
 
-TEST_CASE("test", "STL Container"){
+TEST_CASE("Test", "STL Container"){
 	func<std::vector<int>>();
 	func<std::list<int>>();
 }
